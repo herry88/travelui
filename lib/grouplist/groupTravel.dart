d8bf596af6efd5537f731f28afaed7c35d586789
herry88 <herry081288@gmail.com>
2019-07-31 22:38:26 +0700

    DoneApiGrouplist

--- lib/grouplist/groupTravel.dart
+++ lib/grouplist/groupTravel.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:flutter/material.dart';
 
 class Group {
-  final String id;
-  final String user_id;
+  final int id;
+  final int user_id;
   final String city;
   final String referal;
 
@@ -20,27 +21,62 @@ class Group {
   }
 }
 
-Future<List<Group>> fetchPhotos(http.Client client) async {
-  final response =
-  await client.get('https://jsonplaceholder.typicode.com/photos');
+Future<List<Group>> fetchGroups(http.Client client) async {
+  final response = await client.get('http://hijrah.asia/api/api/user/12/group');
 
   // Use the compute function to run parsePhotos in a separate isolate.
   return compute(parseGroups, response.body);
 }
 
-List<Group>parseGroups(String responseBody){
+List<Group> parseGroups(String responseBody) {
   final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
 
   return parsed.map<Group>((json) => Group.fromJson(json)).toList();
 }
 
 class GroupsListBuilder extends StatelessWidget {
+  final List<Group> groups;
+
+  GroupsListBuilder({Key key, this.groups});
   @override
   Widget build(BuildContext context) {
     return new ListView.builder(
-        itemBuilder: groups.length,
-        
+      itemCount: groups.length,
+      itemBuilder: (context, index) {
+        return new ListTile(
+            title: new Text("Id : ${groups[index].id}\n" +
+                "User Id : ${groups[index].user_id}\n" +
+                "City : ${groups[index].city}\n" +
+                "referal : ${groups[index].referal}"));
+      },
     );
   }
 }
 
+class GroupList extends StatefulWidget {
+  @override
+  _GroupListState createState() => _GroupListState();
+}
+
+class _GroupListState extends State<GroupList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Groups List"),
+      ),
+      body: FutureBuilder<List<Group>>(
+        future: fetchGroups(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+
+          return snapshot.hasData
+              ? GroupsListBuilder(groups: snapshot.data)
+              : Center(
+                  child: CircularProgressIndicator(),
+                );
+        },
+      ),
+    );
+  }
+}
