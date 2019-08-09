16f24447368ada10edcdd8c8dacfd824a1bdd146
aroh <aroh.smkn10@gmail.com>
2019-08-09 11:45:55 +0700

    GalleryPagenew

---
+++ lib/galleryPage/galleryPage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class GalleryPage extends StatefulWidget {
+  final String title; 
+  GalleryPage({this.title}) : super();
+  @override
+  _GalleryPageState createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar : new AppBar(
+        title : new Text(widget.title),
+      ),
+      body: new GridView.extent(
+        maxCrossAxisExtent: 159.0,
+        mainAxisSpacing: 5.0,
+        
+      ),
+    );
+  }
+}
